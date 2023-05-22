e8396eae5bb59fb985b80525881f9cab3b3d1276
shun.uemura <s_uemura@ingalls.co.jp>
2023-05-22 16:39:59 +0900

    再提出_修正

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -71,14 +71,8 @@ public class CreateServlet extends HttpServlet {
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
 
-            em.persist(t);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
-    }
     }
 
 }
